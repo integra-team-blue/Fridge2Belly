a36cf3746f21ee89cf97888014ea7132dc798209
D3moSSS <128296218+D3moSSS@users.noreply.github.com>
2025-09-16 15:08:23 +0300

    #12: Create navbar with Fridge2Belly branding and links to entities

---
+++ frontend/src/app/component/meals-component/meals-component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meals-component',
+  templateUrl: './meals-component.html',
+  styleUrl: './meals-component.css'
+})
+export class MealsComponent {
+
+}
