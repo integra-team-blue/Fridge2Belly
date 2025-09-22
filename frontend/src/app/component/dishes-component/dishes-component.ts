12a8bb0fbb62082f00faba3a92e1b8d704300923
Andrei <andreipuscas2004@gmail.com>
2025-09-22 13:24:02 +0300

    #18:  Displayed Dishes Table View

--- frontend/src/app/component/dishes-component/dishes-component.ts
+++ frontend/src/app/component/dishes-component/dishes-component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 import { Dish, DishesService } from '../../services/dishes-services/dishes-services';
-import { TableModule} from 'primeng/table';
-import { DatePipe, NgForOf, NgIf} from '@angular/common';
+import { TableModule } from 'primeng/table';
+import { DatePipe, NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-dishes',
   templateUrl: './dishes-component.html',
+  styleUrls: ['./dishes-component.css'],
   imports: [
     TableModule,
     DatePipe,
     NgIf,
-    NgForOf,
+    NgForOf
   ],
-  styleUrls: ['./dishes-component.css'],
+  standalone: true
 })
 export class DishesComponent implements OnInit {
   dishes: Dish[] = [];
