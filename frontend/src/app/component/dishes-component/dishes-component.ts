0c304dc6baeaa66e2eb57192a593af6757e18981
Andrei Pușcaș <149624881+andreipuscas0@users.noreply.github.com>
2025-09-22 16:18:29 +0300

    Ap/#18/display dishes table view (#42)

--- frontend/src/app/component/dishes-component/dishes-component.ts
+++ frontend/src/app/component/dishes-component/dishes-component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { Dish, DishesService } from '../../services/dishes-services/dishes-services';
+import { TableModule } from 'primeng/table';
+import { DatePipe, NgForOf, NgIf } from '@angular/common';
 
 @Component({
-  selector: 'app-dishes-component',
+  selector: 'app-dishes',
   templateUrl: './dishes-component.html',
-  styleUrl: './dishes-component.css',
+  styleUrls: ['./dishes-component.css'],
+  imports: [TableModule, DatePipe, NgIf, NgForOf],
+  standalone: true,
 })
-export class DishesComponent {}
+export class DishesComponent implements OnInit {
+  dishes: Dish[] = [];
+
+  constructor(private dishesService: DishesService) {}
+
+  async ngOnInit() {
+    this.dishes = await firstValueFrom(this.dishesService.getDishes());
+    console.log('Dishes loaded:', this.dishes);
+  }
+}
