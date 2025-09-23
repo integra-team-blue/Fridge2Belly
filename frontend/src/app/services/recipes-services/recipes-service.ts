33cb0ab6ffe01df5a85791e341927dba495c5945
Emilia - Andreea Alexandrescu <emilia.alexandrescu@stud.ubbcluj.ro>
2025-09-23 18:23:10 +0300

    Create and Edit Recipe with Shared Entry Form (#44)

--- frontend/src/app/services/recipes-services/recipes-service.ts
+++ frontend/src/app/services/recipes-services/recipes-service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { FormControl, ɵFormGroupRawValue, ɵTypedOrUntyped } from '@angular/forms';
 
 export type Recipe = {
   id: string;
@@ -8,7 +9,7 @@ export type Recipe = {
   description: string;
   cookingTimeMinutes: number;
   instructions: string;
-  ingredientsId: string[];
+  dishIds: string[];
 };
 
 @Injectable({ providedIn: 'root' })
@@ -19,5 +20,42 @@ export class RecipesService {
 
   getRecipes(): Observable<Recipe[]> {
     return this.http.get<Recipe[]>(this.apiUrl);
+  }
+
+  addRecipe(recipe: {
+    instructions: string | undefined;
+    cookingTimeMinutes: number | undefined;
+    name: string | undefined;
+    description: string | undefined;
+    dishIds: string[] | undefined;
+  }): Observable<Recipe> {
+    return this.http.post<Recipe>(this.apiUrl, recipe);
+  }
+
+  updateRecipe(
+    id: string,
+    recipe: ɵTypedOrUntyped<
+      {
+        instructions: FormControl<string>;
+        cookingTimeMinutes: FormControl<number>;
+        name: FormControl<string>;
+        description: FormControl<string>;
+        dishIds: FormControl<string[]>;
+      },
+      ɵFormGroupRawValue<{
+        instructions: FormControl<string>;
+        cookingTimeMinutes: FormControl<number>;
+        name: FormControl<string>;
+        description: FormControl<string>;
+        dishIds: FormControl<string[]>;
+      }>,
+      any
+    >,
+  ): Observable<Recipe> {
+    return this.http.put<Recipe>(`${this.apiUrl}/${id}`, recipe);
+  }
+
+  deleteRecipe(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
