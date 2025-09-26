9003ffb478f52f366d7e34e5d200d4c69a8d1c03
parnau-patrick <parnau.patrick@yahoo.com>
2025-09-26 21:20:42 +0300

    Add Button to Generate Sample Data in Backend 2.0

--- frontend/src/app/component/meals-component/meals-component.ts
+++ frontend/src/app/component/meals-component/meals-component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 import { Meal, MealsService } from '../../services/meals-services/meals-service';
 import { TableModule } from 'primeng/table';
-import { DatePipe, NgForOf } from '@angular/common';
+import { DatePipe, NgForOf ,NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-meals',
   templateUrl: './meals-component.html',
   styleUrls: ['./meals-component.css'],
-  imports: [TableModule, DatePipe, NgForOf],
+  imports: [TableModule, DatePipe, NgForOf,NgIf],
 })
 export class MealsComponent implements OnInit {
   meals: Meal[] = [];
